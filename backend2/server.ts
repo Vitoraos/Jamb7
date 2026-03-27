fb02b76c2c2a2b1da9f5db0566168624ec982d30
Vitora OS <emmykarl07@gmail.com>
2026-03-27 09:43:19 +0100

    Update server.ts

--- backend2/server.ts
+++ backend2/server.ts
@@ -9,7 +9,7 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 if (!GEMINI_API_KEY) {
   throw new Error("GEMINI_API_KEY is not set in environment variables");
 }
-
+const app = express();
 const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
 // Enable CORS
